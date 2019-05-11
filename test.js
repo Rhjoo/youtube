4b18d4216b992ffaedf33dc05fcf8ce2e3e550c5
Ryan Joo <ryanhjoo@gmail.com>
2019-05-11 06:45:35 -0700

    writeFile by year

--- test.js
+++ test.js
@@ -1,6 +1,12 @@
 const _ = require('lodash');
+const fs = require('file-system');
 const data = require('./youtube-watch-history.json');
 
-console.log(data.length);
-console.log(_.filter(data, _.conforms({ "time": (item) =>  _.startsWith(item, "2018")})));
-  
+console.log(data.length);  //23344
+const data2019 = _.filter(data, _.conforms({ "time": (item) =>  _.startsWith(item, "2019")}));
+console.log(data2019.length);
+fs.writeFile(
+  './data2019.json',
+  JSON.stringify(data2019, null, 2),
+  (err) => err ? console.error('Data not written', err) : console.log('Data written')
+);
