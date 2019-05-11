33e16dde7085bce2f949aadd6688f8ad39c1a376
Ryan Joo <ryanhjoo@gmail.com>
2019-05-11 06:24:37 -0700

    filter out by year

--- test.js
+++ test.js
@@ -2,3 +2,5 @@ const _ = require('lodash');
 const data = require('./youtube-watch-history.json');
 
 console.log(data.length);
+console.log(_.filter(data, _.conforms({ "time": (item) =>  _.startsWith(item, "2018")})));
+  
