01950fee0779c9caeac2c26aa908d4a608c0c529
Ryan Joo <ryanhjoo@gmail.com>
2019-05-18 22:51:27 -0700

    remove api key

--- quickstart.js
+++ quickstart.js
@@ -1,6 +1,6 @@
 const request = require('request');
 
-const url = 'https://www.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&id=_NIFz8wRvMs&key=AIzaSyBDg2LMbgpsaLz-JnfRhm8FNINu-lk3rsY';
+const url = 'https://www.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&id=_NIFz8wRvMs&key=[API KEY]]';
 
 request.get(url, (err, res, body) => {
   const json = JSON.parse(body);
